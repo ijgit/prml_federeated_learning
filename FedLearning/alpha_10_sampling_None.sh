3f4c7da2a12b618591b01368f8ccd14c30215079
ijgit <isuu.jeong@gmail.com>
2022-12-09 22:16:51 +0900

    update commands

--- FedLearning/alpha_10_sampling_None.sh
+++ FedLearning/alpha_10_sampling_None.sh
@@ -101,21 +101,20 @@ filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}" 
 python main.py --mp=${is_mp} --method=${method} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
 device=6
-# tm_criterion=CrossEntropyLoss;method=fedprox;mu=1
-# filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}"  #_${sampling}
-# python main.py --mp=${is_mp} --method=${method} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
+tm_criterion=CrossEntropyLoss;method=fedprox;mu=1
+filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}"  #_${sampling}
+python main.py --mp=${is_mp} --method=${method} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
-# tm_criterion=FocalLoss;method=fedprox;mu=1
-# filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}"  #_${sampling}
-# python main.py --mp=${is_mp} --method=${method} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
+tm_criterion=FocalLoss;method=fedprox;mu=1
+filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}"  #_${sampling}
+python main.py --mp=${is_mp} --method=${method} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
-# tm_criterion=Ratio_Cross_Entropy;method=fedprox;mu=1
-# filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}"  #_${sampling}
-# python main.py --mp=${is_mp} --method=${method} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
+tm_criterion=Ratio_Cross_Entropy;method=fedprox;mu=1
+filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}"  #_${sampling}
+python main.py --mp=${is_mp} --method=${method} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
 #################### FedDyn ########################
-device=6
-# device=7
+device=7
 tm_criterion=CrossEntropyLoss;method=feddyn;mu=None
 filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}"  #_${sampling}
 python main.py --mp=${is_mp} --method=${method} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
--- FedLearning/alpha_10_sampling_r_over.sh
+++ FedLearning/alpha_10_sampling_r_over.sh
@@ -103,21 +103,20 @@ filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_$
 python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
 device=6
-# tm_criterion=CrossEntropyLoss;method=fedprox;mu=1
-# filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_${sampling_type}"
-# python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
+tm_criterion=CrossEntropyLoss;method=fedprox;mu=1
+filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_${sampling_type}"
+python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
-# tm_criterion=FocalLoss;method=fedprox;mu=1
-# filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_${sampling_type}"
-# python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
+tm_criterion=FocalLoss;method=fedprox;mu=1
+filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_${sampling_type}"
+python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
-# tm_criterion=Ratio_Cross_Entropy;method=fedprox;mu=1
-# filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_${sampling_type}"
-# python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
+tm_criterion=Ratio_Cross_Entropy;method=fedprox;mu=1
+filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_${sampling_type}"
+python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
 #################### FedDyn ########################
-device=6
-# device=7
+device=7
 tm_criterion=CrossEntropyLoss;method=feddyn;mu=None
 filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_${sampling_type}"
 python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
