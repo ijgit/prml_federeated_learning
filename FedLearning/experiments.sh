8a940a39bf22842df863c36aa198010ab93656b8
ijgit <isuu.jeong@gmail.com>
2022-12-11 01:37:20 +0900

    test ex

--- FedLearning/experiments.sh
+++ FedLearning/experiments.sh
@@ -1,10 +1,10 @@
 bash cifar10-alpha_0.1_sampling_None.sh
 bash cifar10-alpha_0.1_sampling_r_over.sh
 bash cifar10-alpha_0.1_sampling_r_under.sh
+bash cifar10-alpha_1_sampling_None.sh
+bash cifar10-alpha_1_sampling_r_over.sh
+bash cifar10-alpha_1_sampling_r_under.sh
 bash cifar10-alpha_10_sampling_None.sh
 bash cifar10-alpha_10_sampling_r_over.sh
 bash cifar10-alpha_10_sampling_r_under.sh
 bash cifar10-alpha_10_sampling_smote.sh
-bash cifar10-alpha_1_sampling_None.sh
-bash cifar10-alpha_1_sampling_r_over.sh
-bash cifar10-alpha_1_sampling_r_under.sh
