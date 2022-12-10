bd06a12fb5840cc4f554a2f9ba85940bc75ab8e0
ijgit <isuu.jeong@gmail.com>
2022-12-10 22:01:59 +0900

    add commands

--- FedLearning/experiments.sh
+++ FedLearning/experiments.sh
@@ -1,13 +1,10 @@
-bash alpha_0.1_sampling_None.sh
-bash alpha_0.1_sampling_r_over.sh
-bash alpha_0.1_sampling_r_under.sh
-
-bash alpha_1_sampling_None.sh
-bash alpha_1_sampling_r_over.sh
-bash alpha_1_sampling_r_under.sh
-
-# bash alpha_10_sampling_None.sh
-# bash alpha_10_sampling_r_over.sh
-# bash alpha_10_sampling_r_under.sh
-# bash alpha_10_sampling_smote.sh
-
+bash cifar10-alpha_0.1_sampling_None.sh
+bash cifar10-alpha_0.1_sampling_r_over.sh
+bash cifar10-alpha_0.1_sampling_r_under.sh
+bash cifar10-alpha_10_sampling_None.sh
+bash cifar10-alpha_10_sampling_r_over.sh
+bash cifar10-alpha_10_sampling_r_under.sh
+bash cifar10-alpha_10_sampling_smote.sh
+bash cifar10-alpha_1_sampling_None.sh
+bash cifar10-alpha_1_sampling_r_over.sh
+bash cifar10-alpha_1_sampling_r_under.sh
