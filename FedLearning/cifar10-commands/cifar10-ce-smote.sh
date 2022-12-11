b8ddbfd0ee2f6698067a095a03facad7c9f70538
ijgit <isuu.jeong@gmail.com>
2022-12-11 23:44:20 +0900

    update commands

--- FedLearning/cifar10-commands/cifar10-ce-smote.sh
+++ FedLearning/cifar10-commands/cifar10-ce-smote.sh
@@ -19,17 +19,17 @@ div1=0;div2=1;div3=2;div4=3
 # python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
 # alpha=1; device=${div2}
-# tm_criterion=CrossEntropyLoss;method=fedprox;mu=0.0001
+# tm_criterion=CrossEntropyLoss;method=fedavg;mu=None
 # filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_${sampling_type}"
 # python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
 alpha=5; device=${div3}
-tm_criterion=CrossEntropyLoss;method=feddyn;mu=None
+tm_criterion=CrossEntropyLoss;method=fedavg;mu=None
 filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_${sampling_type}"
 python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
 alpha=10; device=${div4}
-tm_criterion=CrossEntropyLoss;method=feddyn;mu=None
+tm_criterion=CrossEntropyLoss;method=fedavg;mu=None
 filename="${seed}_${dataset_name}_${alpha}_${method}(mu:${mu})_${tm_criterion}_${sampling_type}"
 python main.py --mp=${is_mp} --method=${method} --sampling_type=${sampling_type} --tm_mu=${mu} --tm_criterion=${tm_criterion} --tm_local_bs=${tm_local_bs} --device=${device} --alpha=${alpha} --dataset_name=${dataset_name} --num_clients=${num_clients} --fraction=${fraction} --rounds=${rounds} > ./output/${filename}.out 2>&1 &
 
