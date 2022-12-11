686878fa33759f83e37e2f600c0ce2899c066477
ijgit <isuu.jeong@gmail.com>
2022-12-11 23:51:23 +0900

    update commands

--- FedLearning/cifar10-commands/cifar10-experiments-smote.sh
+++ FedLearning/cifar10-commands/cifar10-experiments-smote.sh
@@ -9,18 +9,6 @@
 # bash cifar10-alpha_10_sampling_r_under.sh
 # bash cifar10-alpha_10_sampling_smote.sh
 
-bash cifar10-ce-None.sh
-bash cifar10-rce-None.sh
-bash cifar10-fl-None.sh
-
-bash cifar10-ce-r_under.sh
-bash cifar10-rce-r_under.sh
-bash cifar10-fl-r_under.sh
-
-bash cifar10-ce-augment.sh
-bash cifar10-rce-augment.sh
-bash cifar10-fl-augment.sh
-
 bash cifar10-ce-smote.sh
 bash cifar10-rce-smote.sh
 bash cifar10-fl-smote.sh
